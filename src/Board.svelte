28f84d7551033aaf0a9c08ee9ebf157607a07060
Nico Beierle <nico.beierle@bztes.de>
2024-04-03 13:52:47 +0200

    feat:  UI improvements
    - new colors
    - scrolling
    - button style
    - button title

--- src/Board.svelte
+++ src/Board.svelte
@@ -61,13 +61,15 @@
 
   function snapshotRoundtrip() {
     const snapshot = myUndoStack.createSnapshot({
-      "taskStore": tasksStore
-    })
-    console.log(JSON.stringify(snapshot))
+      taskStore: tasksStore,
+    });
+    console.log(JSON.stringify(snapshot));
     myUndoStack.loadSnapshot(snapshot, {
-      "taskStore": tasksStore
-    })
-    alert('Undo stack was saved to a snapshot, and then restored from the snapshot')
+      taskStore: tasksStore,
+    });
+    alert(
+      'Undo stack was saved to a snapshot, and then restored from the snapshot',
+    );
   }
 
   function clearUndo() {
@@ -81,41 +83,42 @@
 
 <div class="layout">
   <sidebar>
-    <div>
-      <menu>
-        Undo Steps:
-        <div>
-          <button
-            class="icon"
-            on:click={myUndoStack.undo}
-            disabled={!$myUndoStack.canUndo}>undo</button
-          >
-          <button
-            class="icon"
-            on:click={myUndoStack.redo}
-            disabled={!$myUndoStack.canRedo}>redo</button
-          >
-        </div>
-      </menu>
+    <menu>
+      Undo Steps
+      <div>
+        <button
+          class="icon"
+          on:click={myUndoStack.undo}
+          disabled={!$myUndoStack.canUndo}
+          title="undo">undo</button
+        >
+        <button
+          class="icon"
+          on:click={myUndoStack.redo}
+          disabled={!$myUndoStack.canRedo}
+          title="redo">redo</button
+        >
+      </div>
+    </menu>
 
-      <ul class="undo-steps">
+    <div class="history">
+      <ul class="history-list">
         {#each $myUndoStack.actions as undoAction}
-          {@const isUndoStep =
-            undoAction.seqNbr < $myUndoStack.selectedAction.seqNbr}
-          {@const isRedoStep =
-            undoAction.seqNbr > $myUndoStack.selectedAction.seqNbr}
-          {@const isCurrentStep =
-            undoAction.seqNbr === $myUndoStack.selectedAction.seqNbr}
+          {@const step =
+            undoAction.seqNbr < $myUndoStack.selectedAction.seqNbr
+              ? 'undo'
+              : undoAction.seqNbr > $myUndoStack.selectedAction.seqNbr
+                ? 'redo'
+                : 'check'}
+          <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
           <li
-            class:isRedoStep
-            class:isCurrentStep
+            class:isRedoStep={step === 'redo'}
+            class:isCurrentStep={step === 'check'}
             on:click={() => myUndoStack.goto(undoAction.seqNbr)}
-            on:keydown
+            title={step}
           >
             <div>
-              <span class="icon">
-                {isUndoStep ? 'undo' : isRedoStep ? 'redo' : 'check'}
-              </span>
+              <span class="icon">{step}</span>
               {`${undoAction.msg} (${undoAction.seqNbr})`}
             </div>
           </li>
@@ -124,26 +127,30 @@
     </div>
 
     <div class="commands">
-      <button on:click={snapshotRoundtrip}>Create a snapshot and then load it again</button>
+      <button on:click={snapshotRoundtrip}
+        >Create a snapshot and then load it again</button
+      >
       <button on:click={clearUndo}>clearUndo</button>
       <button on:click={clearRedo}>clearRedo</button>
     </div>
   </sidebar>
 
-  <div>
+  <div class="new-task">
     <input
-      class="new-task"
       type="text"
       on:change={(e) => onNewTaskInputChange(e.currentTarget)}
       placeholder="create a new task"
     />
-    <div class="columns">
+  </div>
+
+  <div class="board">
+    <div class="board-columns">
       {#each states as state, stateIndex (state)}
         {@const tasks = Object.values($tasksStore).filter(
           (t) => t.state === state,
         )}
         <div class="column">
-          <h1>{state}</h1>
+          <h2>{state}</h2>
           <ul class="tasks">
             {#each tasks as task (task.id)}
               <li animate:flip={{ duration: 200 }}>
@@ -153,6 +160,7 @@
                 {#if stateIndex > 0}
                   <button
                     class="icon"
+                    title={`move to ${states[stateIndex - 1]}`}
                     on:click={() =>
                       onMoveTaskClick(task, states[stateIndex - 1])}
                   >
@@ -162,6 +170,7 @@
                 {#if stateIndex < states.length - 1}
                   <button
                     class="icon"
+                    title={`move to ${states[stateIndex + 1]}`}
                     on:click={() =>
                       onMoveTaskClick(task, states[stateIndex + 1])}
                   >
@@ -188,43 +197,106 @@
 <style>
   .layout {
     display: grid;
+    grid-template-areas:
+      'sidebar new-task'
+      'sidebar board';
     grid-template-columns: 1fr 3fr;
-    gap: 2rem;
-    height: 100%;
+    grid-template-rows: auto 1fr;
+    height: 100vh;
   }
 
   sidebar {
+    grid-area: sidebar;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    padding: 1rem;
+    background-color: var(--bg-neutral);
+    color: var(--bg-neutral-content);
+    gap: 1rem;
   }
 
   menu {
-    margin: 0 0 1rem 0;
+    margin: 0;
     padding: 0;
     display: flex;
     align-items: center;
     justify-content: space-between;
   }
 
-  div.commands {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 5px;
+  .history {
+    flex: 1;
+    overflow-y: auto;
   }
 
-  .columns {
+  ul.history-list {
+    flex-direction: column-reverse;
+    justify-content: flex-end;
+  }
+
+  ul.history-list li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 0.5rem;
+    font-size: 0.8rem;
+    cursor: pointer;
+  }
+
+  ul.history-list li.isRedoStep {
+    color: #aaa;
+  }
+
+  ul.history-list li.isCurrentStep {
+    font-weight: bold;
+    cursor: default;
+  }
+
+  .commands {
+    display: flex;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+
+  .new-task {
+    grid-area: new-task;
+    font-size: 1.3rem;
+    background-color: var(--bg-base-200);
+    padding: 1rem;
+  }
+
+  .board {
+    grid-area: board;
+    overflow-y: auto;
+  }
+
+  .board-columns {
     display: flex;
     gap: 1rem;
+    padding: 0 1rem;
+    min-height: 100%;
   }
 
   .column {
     flex: 1;
+    justify-content: stretch;
   }
 
-  input.new-task {
-    font-size: 1.3rem;
+  .column:not(:last-child) {
+    border-right: 1px dashed var(--bg-base-300);
+    padding-right: 1rem;
+  }
+
+  h2 {
+    font-weight: lighter;
+    font-size: 1rem;
+    margin: 0;
+    padding: 1rem 0;
+    text-align: center;
+    position: sticky;
+    top: 0;
+    background-color: var(--bg-base-100);
+    z-index: 1;
   }
 
   ul {
@@ -239,11 +311,12 @@
     position: relative;
     margin-bottom: 1rem;
     padding: 0.2rem;
-    background-color: #eee;
-    border: 1px solid #ddd;
+    background-color: var(--bg-base-200);
+    border: 1px solid var(--bg-base-300);
     border-radius: 2px;
     display: flex;
     align-items: center;
+    font-size: 0.8rem;
   }
 
   ul.tasks span.id {
@@ -253,27 +326,5 @@
     font-size: 0.5rem;
     font-family: monospace;
     color: #aaa;
-  }
-
-  ul.undo-steps {
-    flex-direction: column-reverse;
-  }
-
-  ul.undo-steps li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 0.5rem;
-    font-size: 0.8rem;
-    cursor: pointer;
-  }
-
-  ul.undo-steps li.isRedoStep {
-    color: #aaa;
-  }
-
-  ul.undo-steps li.isCurrentStep {
-    font-weight: bold;
-    cursor: default;
   }
 </style>
