6648c44d397113862573a29514ab39075e5a7bae
Nico Beierle <nico.beierle@bztes.de>
2024-11-18 22:37:14 +0100

    chore: upgrade to svelte-5

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './app.css';
+import { mount } from 'svelte';
 import App from './App.svelte';
 
-const app = new App({
+const app = mount(App, {
   // @ts-expect-error bootstrap element is never null
   target: document.getElementById('app'),
 });
